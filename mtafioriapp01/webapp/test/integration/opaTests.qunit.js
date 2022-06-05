b9ccdb897a909fb13fa1fd3295c39b6c06231f2b
CodeWith-Satyajit <officeofsatyajit.com>
2022-06-05 18:23:46 +0000

    Initial Load

---
+++ mtafioriapp01/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ns/mtafiorimanagedapp01/mtafioriapp01/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ mtafioriapp01/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"nsmtafiorimanagedapp01/mtafioriapp01/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
